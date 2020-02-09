4d2ad25538cbb8dfc262e91697c1a95e3c3946e4
rvsbox <rvsbox@gmail.com>
2020-02-09 21:23:38 +0200

    added handlebars, materialize

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express')
-const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
+//установка опций handlebars
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine) //регистрация движка
+app.set('view engine', 'hbs') //использование движка
+app.set('views', 'views') //папка с шаблонами
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+    res.render('about')
 })
 
 
