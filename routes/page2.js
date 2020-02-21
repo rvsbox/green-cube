fac174eb5a0f13135fb7a68cb1f73c14a9a7436a
rvsbox <rvsbox@gmail.com>
2020-02-21 21:32:15 +0200

    information output to page2

--- routes/page2.js
+++ routes/page2.js
@@ -1,11 +1,14 @@
 const {Router} = require('express')
+const ModelPage2 = require('../models/Page2')
 const router = Router()
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const page2 = await ModelPage2.getAll()
   res.render('page2', {
     title: 'Page 2',
-    isPage2: 'false'
+    isPage2: true,
+    page2
   })
 })
 
